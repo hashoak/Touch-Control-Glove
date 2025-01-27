35d3bbf981ef38b37d9201b64124900864535957
Ashok Tirupatimahanti <hash.oak9@gmail.com>
2025-01-27 11:02:26 +0530

    function updates

--- Touch_Control_Glove/Touch_Control_Glove.ino
+++ Touch_Control_Glove/Touch_Control_Glove.ino
@@ -12,12 +12,14 @@ const uint8_t thPin[2] = { 36, 39 };      // Touch pins on thumb finger
 const uint8_t inPin[3] = { 34, 35, 32 };  // Touch pins on index finger
 
 int mt = 500;
-int ttv = 3500;     // Thumb touch trigger value
-int itv = 500;      // Index touch trigger value
-int dtv = 1800;     // Double touch threshold value
-int wt = 100;       // Wait time for next reading
-int cf = 15;        // Cursor movement mitvipier facttvr
-float st = 0.5;     // Touch movement threshold value
+int ttv = 3500;   // Thumb touch trigger value
+int itv = 500;    // Index touch trigger value
+int mtv = 1800;   // Multiple touch threshold value
+int jat = 100;    // Wait time to avoid touch jerk
+int tbm = 500;    // Time before movement
+int rt = 10;      // Touch relaxation time
+int cf = 15;      // Cursor movement multiplier factor
+float st = 0.5;   // Touch movement threshold value
 
 void setup() {
   Serial.begin(115200);
@@ -45,76 +47,111 @@ void setup() {
 }
 
 int t1, t2, i1, i2, i3;
+sensors_event_t a, g, temp;
+unsigned long i,j;
 
 void loop() {
+  // mpu.getEvent(&a, &g, &temp);
+  // Serial.print("rl:-1,ru:1,");
+  // Serial.printf("acc_x:%f,acc_y:%f,acc_z:%f,gyr_x:%f,gyr_y:%f,gyr_z:%f\n",
+  //               a.acceleration.x, a.acceleration.y, a.acceleration.z,
+  //               g.gyro.x, g.gyro.y, g.gyro.z);
+  // delay(100);
   if (!bleKeyboard.isConnected()) ESP.restart();
   t1 = analogRead(thPin[0]);
   t2 = analogRead(thPin[1]);
   i1 = analogRead(inPin[0]);
   i2 = analogRead(inPin[1]);
   i3 = analogRead(inPin[2]);
+  // if (i1 > itv && t1 < ttv) {
   // Serial.print("rl:0,ru:4096,");
   // Serial.printf("t1:%d,t2:%d,i1:%d,i2:%d,i3:%d\n", t1, t2, i1, i2, i3);
-  // delay(wt);
+  // }
   // return;
-  if (t1 < dtv || t2 < dtv) return;
+  if (t1 < mtv || t2 < mtv) return;
   if (i1 > itv && t1 < ttv) {
     Serial.println("Left click");
     Serial.printf("%d\t%d\n", i1, t1);
-    pressAndMove(inPin[0],MOUSE_LEFT);
+    pressMoveRelease(inPin[0], MOUSE_LEFT);
   } else if (i2 > itv && t1 < ttv) {
     Serial.println("Cursor");
     Serial.printf("%d\t%d\n", i2, t1);
-    pressOrMove(inPin[1],MOUSE_LEFT);
+    clickOrVary(inPin[1], MOUSE_LEFT);
   } else if (i3 > itv && t1 < ttv) {
     Serial.println("Right click");
     Serial.printf("%d\t%d\n", i3, t1);
-    pressAndMove(inPin[2],MOUSE_RIGHT);
+    pressMoveRelease(inPin[2], MOUSE_RIGHT);
   } else if (i1 > itv && t2 < ttv) {
     Serial.print("Extra 1  ");
     Serial.printf("%d\t%d\n", i1, t2);
-    delay(wt);
-    if(analogRead(inPin[0]) > itv)
-      while (analogRead(inPin[0]) > itv) cursor();
-    else bleKeyboard.write(KEY_MEDIA_MUTE);
+    clickOrVary(inPin[0], KEY_MEDIA_MUTE, KEY_MEDIA_VOLUME_DOWN, KEY_MEDIA_VOLUME_UP);
   } else if (i2 > itv && t2 < ttv) {
     Serial.print("Extra 2  ");
     Serial.printf("%d\t%d\n", i2, t2);
-    pressAndMove(inPin[0],MOUSE_MIDDLE);
+    pressMoveRelease(inPin[0], MOUSE_MIDDLE);
   } else if (i3 > itv && t2 < ttv) {
     Serial.print("Extra 3  ");
     Serial.printf("%d\t%d\n", i3, t2);
-    delay(wt);
-    if(analogRead(inPin[2]) > itv)
-      while (analogRead(inPin[2]) > itv) cursor();
-    else bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+    clickOrVary(inPin[2], KEY_MEDIA_PLAY_PAUSE, KEY_MEDIA_PREVIOUS_TRACK, KEY_MEDIA_NEXT_TRACK);
   }
 }
 
-sensors_event_t a, g, temp;
-
-inline void pressAndMove(const uint8_t pin, uint8_t action) {
+inline void pressMoveRelease(const uint8_t pin, const uint8_t action) {
   bleMouse.press(action);
-  delay(wt);
-  while (analogRead(pin) > itv) cursor();
+  delay(rt);
+  i = millis()+jat;
+  // Serial.printf("1. %d %d\n",i,millis());
+  while (i > millis() && analogRead(pin) > itv);   // To avoid touch jerk
+  // Serial.printf("2. %d %d\n",i,millis());
+  if (i <= millis()) {                  // If not released soon, start cursor movement
+    delay(rt);
+    // Serial.printf("3. %d %d\n",i,millis());
+    while (analogRead(pin) > itv) {
+      mpu.getEvent(&a, &g, &temp);
+      // Serial.print("rl:-1,ru:1,");
+      // Serial.printf("gyr_x:%f,gyr_y:%f,gyr_z:%f\n",
+      //               g.gyro.x, g.gyro.y, g.gyro.z);
+      bleMouse.move(g.gyro.y * cf, g.gyro.z * cf);
+    }
+  }
   bleMouse.release(action);
+  while(i>millis());
 }
 
-inline void pressOrMove(const uint8_t pin, uint8_t action) {
-    delay(wt);
-    if(analogRead(pin) > itv)
-      while (analogRead(pin) > itv) cursor();
-    else bleMouse.click(action);
+inline void clickOrVary(const uint8_t pin, const uint8_t action) {
+  i = millis()+tbm;
+  j=millis()+jat;
+  // Serial.printf("1. %d %d\n",i,millis());
+  while (analogRead(pin) > itv) {
+    if (j > millis()) continue;
+    mpu.getEvent(&a, &g, &temp);
+    if(g.gyro.y * 5 || g.gyro.z * 5) {            
+      while (analogRead(pin) > itv) {
+        mpu.getEvent(&a, &g, &temp);
+        Serial.print("rl:-0.5,ru:0.5,");
+        Serial.printf("gyr_x:%f,gyr_y:%f,gyr_z:%f\n",
+                      g.gyro.x, g.gyro.y, g.gyro.z);
+        bleMouse.move(g.gyro.y * cf, g.gyro.z * cf);
+      }
+    }
+  }
+  if(i > millis()) bleMouse.click(action);
+  while(j>millis());
 }
 
-void cursor() {
-  mpu.getEvent(&a, &g, &temp);
-  Serial.print("rl:-10,ru:10,");
-  Serial.printf(
-      "acc_x:%f,acc_y:%f,acc_z:%f,gyr_x:%f,gyr_y:%f,gyr_z:%f\n",
-      a.acceleration.x,a.acceleration.y,a.acceleration.z,
-      g.gyro.x,g.gyro.y,g.gyro.z
-    );
-  if (g.gyro.z < -st || g.gyro.z > st || g.gyro.y < -st || g.gyro.y > st)
-    bleMouse.move(g.gyro.y * cf, g.gyro.z * cf);
+inline void clickOrVary(const uint8_t pin, const MediaKeyReport action, const MediaKeyReport actionL, const MediaKeyReport actionR) {
+  while(i + tbm > millis() && analogRead(pin) > itv);
+  
+  
+  delay(jat);
+  if (analogRead(pin) > itv)
+    while (analogRead(pin) > itv) {
+      mpu.getEvent(&a, &g, &temp);
+      // Serial.print("rl:-1,ru:1,");
+      // Serial.printf("acc_x:%f,acc_y:%f,acc_z:%f,gyr_x:%f,gyr_y:%f,gyr_z:%f\n",
+      //               a.acceleration.x, a.acceleration.y, a.acceleration.z,
+      //               g.gyro.x, g.gyro.y, g.gyro.z);
+      bleMouse.move(g.gyro.y * cf, g.gyro.z * cf);
+    }
+  else bleKeyboard.write(action);
 }
